606a79ca4bb4d43c1122d09433023381309ac977
Joseph Abidoye <changedmantme@gmail.com>
2023-03-16 06:09:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts() function
+ *
+ * Return: Return 0 Always (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
