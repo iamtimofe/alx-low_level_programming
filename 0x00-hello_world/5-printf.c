13913ecdbd61385617de9f7054da2d47987cf395
Joseph Abidoye <changedmantme@gmail.com>
2023-03-16 06:32:52 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with printf() function
+ *
+ * Return: Returns 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
