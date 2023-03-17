5385eca0ee44c08f979aec109e8997de56bf9091
Joseph Abidoye <changedmantme@gmail.com>
2023-03-17 09:11:53 -0700

    Printing alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabelt in lower case
+ *
+ * Returns: Returns 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
