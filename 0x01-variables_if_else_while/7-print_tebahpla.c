74259012b1d5813d8191a86d2439df98219c252b
Joseph Abidoye <changedmantme@gmail.com>
2023-03-17 14:28:41 -0700

    Reverse printing of alphabet in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Level
+ *Description - Prints lowercase alphabets inreverse
+ *
+ * Return: Return 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z' i >= 'a' i--)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
