cb887f595ce3896c29dda9c3d20b63952a3d733b
Joseph Abidoye <changedmantme@gmail.com>
2023-03-23 04:11:46 -0700

    Nested Functions and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies a and b
+ * @a: variable
+ * @b: variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#Nested LOOP CONTINUATION
