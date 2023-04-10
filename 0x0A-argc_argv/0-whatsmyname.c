6290fb683f606d00afab4a4587d5c62f82c8965b
Joseph Abidoye <changedmantme@gmail.com>
2023-04-10 13:03:15 -0700

    Arguement

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: parameter count
+ * @argv: array
+ * Return: Returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
