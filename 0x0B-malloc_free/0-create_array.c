c7f283c0631ac4c88ac650a80cf1ef61aa2a069c
Joseph Abidoye <changedmantme@gmail.com>
2023-04-11 14:29:01 -0700

    Array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,9 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
-	if (size == 0 && arr == 0)
+	if (size == 0 || arr == 0)
 	{
-		return (NULL);
+		return (0);
 	}
 	for (i = 0; i < size; i++)
 	{
